7cb3672cded198897e05d7a51285f352427d850b
zac <s516936@nwmissouri.edu>
2019-12-02 21:47:28 -0600

    Initial Commit

---
+++ CameraTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CameraTest
+//
+//  Created by Student on 12/2/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
